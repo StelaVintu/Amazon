b826fe102265c466da06a45cea9a13f62519be5b
Stela Vintu <vintustela@yahoo.com>
2024-02-22 22:29:04 -0800

    amazon tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -25,31 +25,38 @@ describe("Login", () => {
   });
 
   it("Should login, verify greeting message, and logout if no CAPTCHA present", () => {
-    HomePage.loginBtn.should('be.visible', { timeout: 10000 }).click();
+    HomePage.loginBtn.should("be.visible", { timeout: 10000 }).click();
     LoginPage.loginWithCredentials(userData.email, userData.password);
     cy.wait(2000);
-    cy.url().should('eq', urls.accountUrl).then((url) => {
-      if (url.includes(urls.accountUrl)) {
-        AccountPage.profileIcon.should("have.text", `Hello, ${userData.firstName}`);
-        AccountPage.accountBtn.trigger('mouseover');
-        cy.wait(2000)
-        AccountPage.accountBtnDropdownMenu.last().click({ force: true });
-        cy.wait(1000)
-        cy.url().should('eq', urls.signinUrl);
-      } else {
-        cy.url().should('eq', urls.captchaUrl);
-        cy.log('CAPTCHA is in place');
-        CaptchaPage.captchaIframeLogin.within(() => {
-          expect(CaptchaPage.captchaIframeLogin).to.exist;
-          const $doc = CaptchaPage.captchaIframeLogin.contents();
-          const $element = $doc.find(CaptchaPage.captchaHeaderLogin);
-          expect($element).to.exist;
-          const text = $element.text().trim();
-          expect(text).to.equal(verificationText.captchaText);
-        }).catch((error) => {
-          cy.log('Error:', error.message);
-        });
-      }
-    });
+    cy.url()
+      .should("eq", urls.accountUrl)
+      .then((url) => {
+        if (url.includes(urls.accountUrl)) {
+          AccountPage.profileIcon.should(
+            "have.text",
+            `Hello, ${userData.firstName}`
+          );
+          AccountPage.accountBtn.trigger("mouseover");
+          cy.wait(2000);
+          AccountPage.accountBtnDropdownMenu.last().click({ force: true });
+          cy.wait(1000);
+          cy.url().should("eq", urls.signinUrl);
+        } else {
+          cy.url().should("eq", urls.captchaUrl);
+          cy.log("CAPTCHA is in place");
+          CaptchaPage.captchaIframeLogin
+            .within(() => {
+              expect(CaptchaPage.captchaIframeLogin).to.exist;
+              const $doc = CaptchaPage.captchaIframeLogin.contents();
+              const $element = $doc.find(CaptchaPage.captchaHeaderLogin);
+              expect($element).to.exist;
+              const text = $element.text().trim();
+              expect(text).to.equal(verificationText.captchaText);
+            })
+            .catch((error) => {
+              cy.log("Error:", error.message);
+            });
+        }
+      });
   });
 });
